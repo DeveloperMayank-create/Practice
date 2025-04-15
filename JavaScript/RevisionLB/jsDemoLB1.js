02d6be12a82ad44036b6b3de64aea98b2e811f65
DeveloperMayank-create <mayank.mishra.fullstackdeveloper@gmail.com>
2025-04-15 12:33:31 +0530

    added first Demo file of jsDemoLB series

---
+++ JavaScript/RevisionLB/jsDemoLB1.js
@@ -0,0 +1,10 @@
+// JS is a programming language but it is more over a scripting language, script is a set of programming lines by which we can perform any task.
+// At begining it was client side language but after the C++ and JS integration it also became the server side language.
+// ECMA is an statnderd of building the set of rules and regulations of JS ,so it can provide the linear code everywhere .
+// Initially Moka was the name of JS then it became LiveScript and at last it became JavaScript.
+// JS runs on browser by using JS Engine ,For Chrome it uses V8 engine ad Firefox uses SpiderMonkey engingn
+// To view the results into terminal , we need to run 'node fileName.js' into terminal (JAVASRIPT FILE)
+// Console.log("Hii") means that print (.log) "Hii"(message) on the page(console).
+
+
+console.log("Hello Mayank , How are you !!!");
